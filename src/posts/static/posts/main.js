9d11f43eea4cbb1fe953e7e92aedafbb5e9d7dfa
wgbnlee <wgbnlee@gmail.com>
2025-04-11 16:23:52 -0400

    Modify views.py, urls.py and main.js in posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -3,3 +3,14 @@ console.log('hello world')
 const helloWorldBox = document.getElementById('hello-world')
 
 helloWorldBox.innerHTML = 'hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
