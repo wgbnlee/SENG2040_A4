d54ccf1d6d173e725c0bbb03616fcb9eba5ca580
wgbnlee <wgbnlee@gmail.com>
2025-04-11 14:32:23 -0400

    Updated settings.py

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Our own apps
+    'posts',
+    'profiles',
+    # 3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'posts_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
