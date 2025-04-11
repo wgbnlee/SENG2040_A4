f0e2f907f328978064c554a9383d68c40970df9d
wgbnlee <wgbnlee@gmail.com>
2025-04-11 16:25:00 -0400

    Update main.js

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,13 +2,13 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 
-helloWorldBox.innerHTML = 'hello <b>world</b>'
 
 $.ajax({
     type: 'GET',
     url: '/hello-world/',
     success: function(response){
-        console.log('success', response)
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
     },
     error: function(error){
         console.log('error', error)
