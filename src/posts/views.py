425321748bfec50c895aead9047f2aff757716eb
wgbnlee <wgbnlee@gmail.com>
2025-04-11 16:30:43 -0400

    Modify veiws.py, urls.py, main.html in posts
    Make Json call for post data

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from django.core import serializers
 # Create your views here.
 
 def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
+
+def load_post_data_view(request):
+    qs = Post.objects.all()
+    data =serializers.serialize('json', qs)
+    return JsonResponse({'data':data})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
