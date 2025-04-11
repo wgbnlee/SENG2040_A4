db4c56b6c9f0420e4c7ca02eb2de4dca18707d30
wgbnlee <wgbnlee@gmail.com>
2025-04-11 16:40:49 -0400

    List post data
    Created a dictionary to return as a JSON response. In main.js, foreach loop to add each JSON data as data is an array.

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 
 $.ajax({
@@ -12,5 +13,23 @@ $.ajax({
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log(error)
     }
 })
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
-from django.core import serializers
 # Create your views here.
 
 def post_list_and_create(request):
@@ -10,7 +9,15 @@ def post_list_and_create(request):
 
 def load_post_data_view(request):
     qs = Post.objects.all()
-    data =serializers.serialize('json', qs)
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'author': obj.author.user.username
+        }
+        data.append(item)
     return JsonResponse({'data':data})
 
 def hello_world_view(request):
