27d2666e20522758fb3835bae7cd76ace14ee00e
wgbnlee <wgbnlee@gmail.com>
2025-04-11 20:48:36 -0400

    Added spinner-box and timer

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 
 $.ajax({
@@ -22,12 +23,15 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
             postsBox.innerHTML += `
                 ${el.title} - <b>${el.body}</b><br>
             `
-        });
+            });
+        }, 1000)
     },
     error: function(error){
         console.log(error)
