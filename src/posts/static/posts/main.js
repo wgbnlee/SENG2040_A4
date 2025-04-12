d1aaaad6ae53375ebb08f21cf748b026e9494082
wgbnlee <wgbnlee@gmail.com>
2025-04-11 23:56:39 -0400

    Add functionality to reset form after submit

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -148,6 +148,7 @@ postForm.addEventListener('submit', e=>{
                 likeUnlikePosts()
                 $('#addPostModal').modal('hide')
                 handleAlerts('success', 'New post added!')
+                postForm.reset()
         },
         error: function(error){
             console.log(error)
