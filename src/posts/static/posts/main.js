078944c4b38b97254f226e0493a188a4de04454a
wgbnlee <wgbnlee@gmail.com>
2025-04-11 21:04:31 -0400

    Add Load more button

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -17,38 +17,42 @@ $.ajax({
     }
 })
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
+let visible = 3
+
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `
-            });
-        }, 1000)
-    },
-    error: function(error){
-        console.log(error)
-    }
-})
+                    `
+                });
+            }, 1000)
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+}
