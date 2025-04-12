9a37c89726b6e2b5cf066f2d4a95e496b5811e12
wgbnlee <wgbnlee@gmail.com>
2025-04-11 22:25:42 -0400

    Modify Setting.py and Create forms.py

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'profiles',
     # 3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
