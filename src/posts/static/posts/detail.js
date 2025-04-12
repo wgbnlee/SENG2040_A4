90fb44add43b7b5143020b8ae2e41d37b3666078
wgbnlee <wgbnlee@gmail.com>
2025-04-12 02:28:20 -0400

    Update delete button

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,8 +1,14 @@
 console.log('hello world detail')
 
+const postBox = document.getElementById('post-box')
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
+
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
 
 backBtn.addEventListener('click', ()=>{
     history.back()
@@ -13,6 +19,30 @@ $.ajax({
     url: url,
     success: function(response){
         console.log(response)
+        const data = response.data
+        if (data.logged_in !== data.author) {
+            console.log('different')
+        } else {
+            console.log('the same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-3')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
